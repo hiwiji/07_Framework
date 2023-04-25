a122f48f76b8e1ebb83e68ac7cf57a08a4395aba
hiwiji <jjewwi12@gmail.com>
2023-04-25 23:36:31 +0900

    230424 study

--- comm/src/main/java/edu/kh/comm/member/controller/MemberController.java
+++ comm/src/main/java/edu/kh/comm/member/controller/MemberController.java
@@ -1,17 +1,23 @@
 package edu.kh.comm.member.controller;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.kh.comm.member.model.service.MemberService;
 import edu.kh.comm.member.model.vo.Member;
@@ -34,6 +40,9 @@ import edu.kh.comm.member.model.vo.Member;
 //localhost:8080/comm/member/login
 //localhost:8080/comm/member/signUp
 
+@SessionAttributes({"loginMember"}) // 자바 배열형태 {} 로 넣기 
+											// Model에 추가된 값의 key와 어노테이션에 작성된 값이 같으면
+											// 해당 값을 session scope로 이동시키는 역할
 
 public class MemberController {
 	
@@ -134,7 +143,7 @@ public class MemberController {
 	// -> 파라미터 중 name 속성값이 VO의 필드와 일치하면
 	// 해당 VO 객체의 필드에 값을 세팅
 	
-	// *** @ModelAttribute를 이용해서 객체에 갑슬 직접 담는 경우에 대한 주의 사항 ***
+	// *** @ModelAttribute를 이용해서 객체에 값을 직접 담는 경우에 대한 주의 사항 ***
 	// -- 반드시 필요한 내용
 	// - VO의 기본 생성자
 	// - VO 필드에 대한 Setter , NoArgsConstructor
@@ -143,15 +152,90 @@ public class MemberController {
 	
 	
 	@PostMapping("/login")
-	public String login( @ModelAttribute Member inputMember ) {
-		
+	public String login( /*@ModelAttribute */ Member inputMember, 
+						Model model,
+						RedirectAttributes ra,
+						HttpServletResponse resp,
+						HttpServletRequest req,
+						@RequestParam(value="saveId", required=false) String saveId ) {
+	
+		// 커맨드 객체
+		// @ModelAttribute 생략된 상태에서 파라미터가 필드에 세팅된 객체
+	
 		logger.info("로그인 기능 수행됨");
 		
 		// 아이디, 비밀번호가 일치하는 회원 정보를 조회하는 Service 호출 후 결과 반환 받기
 		Member loginMember = service.login(inputMember);
 		
+		
+		/* Model : 데이터를 맵 형식(K:V) 형태로 담아 전달하는 용도의 객체
+		 * -> request, session을 대체하는 객체
+		 * 
+		 * - 기본 scope : request
+		 * - session scope로 변환하고 싶은 경우
+		 *   클래스 레벨로 @SessionAttributes를 작성하면 된다.
+		 *   
+		 *   @SessionAttributes 미작성 -> request scope
+		 * 
+		 * */
+	    if ( loginMember != null) { // 로그인 성공시
+	    	  model.addAttribute("loginMember", loginMember);  // ==req.setAttribute("loginMember", loginMember);	
+	    	  
+	    	  // 로그인 성공시 무조건 쿠키 생성
+	    	  // 단, 아이디 저장 체크 여부에 따라서 쿠키의 유지시간을 조정
+	    	  
+	    	  Cookie cookie = new Cookie("saveId", loginMember.getMemberEmail());
+	    	  // 쿠키는 기존과 동일하게 사용 new를 아예 안쓰는게 아니라 거의 안쓰는거임
+	    	 
+	    	  if (saveId != null ) { // 아이디 저장이 체크되어있을 때
+	    		  
+	    		  cookie.setMaxAge(60 * 60 * 24 * 365); // 초단위 지정(1년)
+	    		  
+	    	  } else { // 체크되지 않았을 때
+	    		  
+	    		  cookie.setMaxAge(0); // 0초 -> 생성되자마자 사라짐 == 쿠키삭제
+	    	  }
+	    	  
+	    	  // 쿠키가 적용될 범위(경로) 지정
+	    	  cookie.setPath(req.getContextPath());   // /comm 안에서 모두 쿠키 적용하겠다는 말
+	    	  
+	    	  // 쿠키를 응답 시 클라이언트에게 전달
+	    	  resp.addCookie(cookie);
+	    	  
+	    
+	    } else {
+	    	// model.addAttribute("message", "아이디 또는 비밀번호가 일치하지 않습니다.");
+	    	
+	    	ra.addFlashAttribute("message", "아이디 또는 비밀번호가 일치하지 않습니다.");
+	    	
+	    	// redirect 시에도 request scope로 세팅된 데이터가 유지될 수 있도록 하는 방법을
+	    	// Spring 에서 제공해줌
+	    	// RedirectAttributes객체 (컨트롤러 매개변수에 작성하면 사용 가능)
+	    }
+		
 		return "redirect:/"; 
 	} 
+	
+	//로그아웃
+	@GetMapping("/logout")
+	public String logout(/*HttpSession session*/
+						SessionStatus status ) {
+		// 로그아웃 == 세션을 없애는 것
+		
+		// * @SessionAttributes을 이용해서 session scope에 배치된 데이터는
+		//   SessionStatus라는 별도 객체를 이용해야만 없앨 수 있다.
+		
+		logger.info("로그아웃 수행됨");
+		
+		// session.invalidate(); 기존 세션 무효화 방식으로는 X
+		
+		status.setComplete(); // 세션이 할 일이 완료됨 -> 없앰
+		
+		return "redirect:/";  // 메인페이지 재요청
+	} 
+	
+	
+	
 	
 	
 	// 회원 가입 화면 전환
